3386ef83a31c2e271ec7f6008de94dbf447f0085
gerrit <gerrit.leuchner@gmail.com>
2017-05-11 20:42:35 +0200

    import, gitignore

--- src/main/java/de/hska/muon/ZuulServerApplication.java
+++ src/main/java/de/hska/muon/ZuulServerApplication.java
@@ -2,6 +2,7 @@ package de.hska.muon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
