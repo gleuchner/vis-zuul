e7954188932d685dfd7721f569d938c53a72dee3
gerrit <gerrit.leuchner@gmail.com>
2017-06-21 23:53:10 +0200

    oauth

--- src/main/java/de/hska/muon/ZuulServerApplication.java
+++ src/main/java/de/hska/muon/ZuulServerApplication.java
@@ -4,13 +4,29 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableDiscoveryClient
+@EnableResourceServer
 public class ZuulServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulServerApplication.class, args);
+	}
+
+	@Primary
+	@Bean
+	public RemoteTokenServices tokenService() {
+		RemoteTokenServices tokenService = new RemoteTokenServices();
+		tokenService.setCheckTokenEndpointUrl(
+				"http://authserver:8763/oauth/check_token");
+		tokenService.setClientId("my-client-with-secret");
+		tokenService.setClientSecret("secret");
+		return tokenService;
 	}
 }
